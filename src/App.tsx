b09ed304a44e7f03de55666e06b2f154def7edcc
Felipe Sales <felipefsp.dev@gmail.com>
2023-10-28 08:41:50 -0300

    chore: installed react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './Style.css';
 import SideNav from './components/SideNav';
 
 function App() {
   return (
     <>
-      <div className='flex box'>
-        <SideNav />
-      </div>
+      <BrowserRouter>
+        <div className='flex box'>
+          <SideNav />
+          <main>
+            <Routes></Routes>
+          </main>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
